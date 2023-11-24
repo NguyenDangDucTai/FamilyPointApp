cd76fd711649388fb56cab1d19b4f65375f22d3d
ThuHuynh2809 <124646995+ThuHuynh2809@users.noreply.github.com>
2023-11-24 23:23:48 +0700

    commit trang chủ

--- FamilyPointApp/screen/ThongTinCaNhan.js
+++ FamilyPointApp/screen/ThongTinCaNhan.js
@@ -3,8 +3,76 @@ import { StyleSheet, Image, Text, View, TextInput, TouchableOpacity } from 'reac
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Icon from "react-native-vector-icons/FontAwesome";
+import React, { useState, useEffect, useCallback } from "react";
 
-export default function ThongTinCaNhan({ navigation }) {
+export default function ThongTinCaNhan({ navigation, route }) {
+
+    const [data, setData] = useState([]);
+    const { userPhone } = route.params;
+    const [user, setUser] = useState({
+        id: "",
+        name: "",
+        phone: "",
+        sex: "",
+        birthday: "",
+        country: "",
+        address: "",
+        // ... other fields
+    });
+    useEffect(() => {
+        fetch("https://656047f683aba11d99d086dc.mockapi.io/users")
+            .then((response) => response.json())
+            .then((data) => {
+                const user = data.find((user) => user.phone === userPhone);
+                setUser(user.id);
+                setData(user);
+            })
+            .catch((error) => console.error("Lỗi khi lấy dữ liệu:", error));
+    }, [userPhone]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch("https://656047f683aba11d99d086dc.mockapi.io/users");
+                if (!response.ok) {
+                    throw new Error("Failed to fetch data");
+                }
+                const data = await response.json();
+                const userData = data.find((userData) => userData.phone === userPhone);
+
+                if (userData) {
+                    setUser(userData);
+                }
+            } catch (error) {
+                console.error("Error fetching data:", error);
+            }
+        };
+
+        fetchData();
+    }, [userPhone]);
+
+    const updateUserData = async () => {
+        try {
+            const response = await fetch(`https://656047f683aba11d99d086dc.mockapi.io/users/${user.id}`, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(user),
+            });
+
+            if (!response.ok) {
+                throw new Error(`HTTP error! Status: ${response.status}`);
+            }
+
+            const updatedUserData = await response.json();
+            setUser(updatedUserData);
+        } catch (error) {
+            console.error("Error updating user data:", error);
+        }
+        navigation.navigate("TrangChu", { userPhone: userPhone });
+    };
+
     return (
         <View style={styles.container}>
             <View style={styles.header}>
@@ -17,11 +85,12 @@ export default function ThongTinCaNhan({ navigation }) {
             </View>
             <View style={styles.body}>
                 <View style={styles.viewTitle}>
-                    <Image source={require('../assets/ThongTinCaNhan/image 3.png')}
+                    <Image  source={{ uri: data?.avatar }}
                         style={styles.imgAvata}
                     />
                 </View>
-                <TouchableOpacity style={styles.btnDoiMaPin}>
+                <TouchableOpacity style={styles.btnDoiMaPin}
+                    onPress={() => navigation.navigate("DoiMaPin",{ userPhone: userPhone })}>
                     <Image source={require('../assets/ThongTinCaNhan/image 18.png')}
                         style={styles.imgLock}
                     />
@@ -29,37 +98,51 @@ export default function ThongTinCaNhan({ navigation }) {
                 </TouchableOpacity>
                 <View style={styles.viewInput}>
                     <Text style={styles.textTitle2}>Họ tên</Text>
-                    <TextInput style={styles.textInput}></TextInput>
+                    <TextInput style={styles.textInput}
+                        value={user.name}
+                        onChangeText={(text) => setUser({ ...user, name: text })}></TextInput>
                 </View>
                 <View style={styles.viewInput}>
                     <Text style={styles.textTitle2}>Số điện thoại</Text>
-                    <TextInput style={styles.textInput}></TextInput>
+                    <TextInput style={styles.textInput}
+                        value={user.phone}
+                        onChangeText={(text) => setUser({ ...user, phone: text })}></TextInput>
                 </View>
                 <View style={styles.viewInput}>
                     <Text style={styles.textTitle2}>Giới tính</Text>
-                    <TextInput style={styles.textInput}></TextInput>
+                    <TextInput style={styles.textInput}
+                        value={user.sex}
+                        onChangeText={(text) => setUser({ ...user, sex: text })}></TextInput>
                 </View>
                 <View style={styles.viewInput}>
                     <Text style={styles.textTitle2}>Ngày sinh</Text>
-                    <TextInput style={styles.textInput}></TextInput>
+                    <TextInput style={styles.textInput}
+                        value={user.birthday}
+                        onChangeText={(text) => setUser({ ...user, birthday: text })}></TextInput>
                 </View>
                 <View style={styles.viewInput}>
                     <Text style={styles.textTitle2}>Quốc tịch</Text>
-                    <TextInput style={styles.textInput}></TextInput>
+                    <TextInput style={styles.textInput}
+                        value={user.country}
+                        onChangeText={(text) => setUser({ ...user, contry: text })}></TextInput>
                 </View>
                 <View style={styles.viewInput}>
                     <Text style={styles.textTitle2}>Địa chỉ</Text>
-                    <TextInput style={styles.textInput}></TextInput>
+                    <TextInput style={styles.textInput}
+                        value={user.address}
+                        onChangeText={(text) => setUser({ ...user, address: text })}></TextInput>
                 </View>
             </View>
             <View style={styles.footer}>
-                <TouchableOpacity style={styles.btnCancel}>
-                    <Image source={require('../assets/DangKy/cancel.png')}
+                <TouchableOpacity style={styles.btnCancel}
+                    onPress={() => navigation.navigate("TrangChu", { userPhone: userPhone })}>
+                    <Image source={require('../assets/ThongTinCaNhan/cancel.png')}
                         style={styles.imgCancel}
                     />
                     <Text style={styles.textCancel}>Hủy bỏ</Text>
                 </TouchableOpacity>
-                <TouchableOpacity style={styles.btnCapNhat}>
+                <TouchableOpacity style={styles.btnCapNhat}
+                    onPress={updateUserData}>
                     <Text style={styles.textCapNhat}>Cập nhật</Text>
                 </TouchableOpacity>
             </View>
@@ -98,6 +181,7 @@ const styles = StyleSheet.create({
     imgAvata: {
         width: 60,
         height: 60,
+        borderRadius: 100,
         marginHorizontal: 10
     },
     btnDoiMaPin: {
@@ -118,8 +202,13 @@ const styles = StyleSheet.create({
         height: '75%',
     },
     viewTitle: {
+        width: 70,
+        height: 70,
         flexDirection: 'row',
         alignItems: 'center',
+        justifyContent:'center',
+        backgroundColor: '#D9D9D9',
+        borderRadius: 100,
     },
     textTitle: {
         color: '#008CD7',
@@ -185,7 +274,7 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         alignItems: 'center',
         justifyContent: 'center',
-        marginVertical:10
+        marginVertical: 10
     },
     textCapNhat: {
         fontSize: 18,
